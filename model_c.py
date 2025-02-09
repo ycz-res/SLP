51738d813a491c94301a5448d7451b4ca4b06adc
SoftpaseFar <kang_hui1314@126.com>
2025-02-09 18:29:21 +0800

    完善模型

---
+++ model_c.py
@@ -0,0 +1,134 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# 文本编码器 cell
+class EncoderLayer(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderLayer, self).__init__()
+        self.hidden_size = hidden_size
+
+        # 过滤门
+        self.Wf = nn.Linear(input_size + hidden_size, hidden_size)
+        self.bf = nn.Parameter(torch.randn(hidden_size))
+
+        # 注意力
+        self.Wq = nn.Linear(hidden_size, hidden_size)
+        self.Wk = nn.Linear(input_size + hidden_size, hidden_size)
+        self.Wv = nn.Linear(input_size + hidden_size, hidden_size)
+
+        # 更新门
+        self.Wu = nn.Linear(hidden_size, hidden_size)
+        self.bu = nn.Parameter(torch.randn(hidden_size))
+
+    def forward(self, xt, ht_1, ct_1, et_1):
+        # concat
+        concat = torch.cat((xt, ht_1), dim=1)
+
+        # ct_1_hidden
+        ft = torch.sigmoid(self.Wf(concat) + self.bf)
+        ct_1_hidden = ft * ct_1
+
+        Q = self.Wq(ct_1_hidden)
+        K = self.Wk(concat)
+        V = self.Wv(concat)
+
+        # 计算注意力得分
+        attention_scores = torch.matmul(Q, K.transpose(-2, -1)) / (self.hidden_size ** 0.5)
+        attention_weights = F.softmax(attention_scores, dim=-1)
+
+        # 应用注意力权重到V
+        attention_output = torch.matmul(attention_weights, V).squeeze(1)
+
+        ut = torch.sigmoid(self.Wu(attention_output) + self.bu)
+        et = ut * et_1
+
+        ct = ct_1_hidden + attention_output
+        ht = attention_output
+
+        return ht, ct, et
+
+
+# 文本编码器
+class Encoder(nn.Module):
+    def __init__(self, hidden_size=64, num_layers=1):
+        super(Encoder, self).__init__()
+        self.hidden_size = hidden_size
+
+        # num_layers 个 EncoderLayer
+        self.cells = nn.ModuleList(
+            [EncoderLayer(hidden_size, hidden_size) for _ in range(num_layers)]
+        )
+
+    def forward(self, src):
+        batch_size, seq_length, _ = src.size()
+        ht_prev = torch.zeros(batch_size, self.hidden_size).to(src.device)
+        ct_prev = torch.zeros(batch_size, self.hidden_size).to(src.device)
+        et_prev = torch.zeros(batch_size, self.hidden_size).to(src.device)
+
+        for t in range(seq_length):
+            xt = src[:, t, :]
+
+            for cell in self.cells:
+                ht_prev, ct_prev, et_prev = cell(xt, ht_prev, ct_prev, et_prev)
+
+        ht = ht_prev
+        ct = ct_prev
+        et = et_prev
+        return ht, ct, et
+
+
+# 姿态解码模型
+class EmoGene(nn.Module):
+    def __init__(self, src_dim=64, model_dim=64, tgt_dim=274, kp_nums=137,
+                 scale=800, num_heads=2, num_layers=1, seq_length=156,
+                 num_embeddings=30522):
+        super(EmoGene, self).__init__()
+        # self.scale = scale
+        self.seq_length = seq_length
+        self.src_embedding = nn.Embedding(num_embeddings, src_dim)
+        self.position_embedding = torch.nn.Embedding(num_embeddings=seq_length, embedding_dim=model_dim)
+
+        self.encoder = Encoder(hidden_size=model_dim, num_layers=num_layers)
+        # 注意力
+        self.Wq = nn.Linear(model_dim, model_dim)
+        self.Wk = nn.Linear(model_dim + model_dim, model_dim)
+        self.Wv = nn.Linear(model_dim + model_dim, model_dim)
+
+        # 输入
+        self.src_linear = nn.Linear(src_dim, model_dim)
+        # Decoder的输入
+        self.tgt_linear = nn.Linear(tgt_dim, model_dim)
+        # Decoder的输出
+        self.output_linear = nn.Linear(model_dim, kp_nums * 3)
+
+        self.mha = nn.MultiheadAttention(embed_dim=model_dim, num_heads=num_heads)
+
+    def forward(self, src, tgt):
+        # 获取 ht、et
+        src = self.src_embedding(src['input_ids'])
+        src = self.src_linear(src)
+        ht, _, et = self.encoder(src)
+
+        # 位置编码
+        position_indices = torch.arange(self.seq_length).unsqueeze(0).expand(tgt.size(0), -1)  # 形状为 [2, 156]
+        pos = self.position_embedding(position_indices)  # 形状为 [2, 156, 64]
+
+        # 注意力
+        Q = self.Wq(ht)
+        K = self.Wk(ht)
+
+        tgt = self.tgt_linear(tgt['input_ids'])
+        et_expanded = et.unsqueeze(1)
+        V = tgt + et_expanded + pos
+        V = self.Wv(V)
+
+        output = self.mha(Q, K, V)
+
+        print('output.shape:', output.shape)
+
+        return output
+
+# 结果评价模型
+# 待写
---
+++ train_c.py
@@ -0,0 +1,234 @@
+# 忽略警告---
+import warnings
+
+warnings.filterwarnings('ignore')
+# ---忽略警告
+
+import utils
+import torch
+import argparse
+import random
+import numpy as np
+import os
+from pathlib import Path
+from transformers import AutoTokenizer, MBartTokenizer
+from torch.utils.data import DataLoader
+from model_c import EmoGene
+# 动态调用引入 How2SignDataset
+from dataset import How2SignDataset
+from timm.optim import create_optimizer
+from torch.optim import lr_scheduler as scheduler
+from torch.cuda.amp import GradScaler, autocast
+from loss import MAELoss
+
+
+def get_args_parser():
+    a_parser = argparse.ArgumentParser('SLP scripts', add_help=False)
+    a_parser.add_argument('--batch_size', default=1, type=int)
+    a_parser.add_argument('--epochs', default=100, type=int)
+    a_parser.add_argument('--num_workers', default=1, type=int)
+    a_parser.add_argument('--config', type=str, default='./config.yaml')
+    a_parser.add_argument('--device', default='cpu')
+    a_parser.add_argument('--seed', default=0, type=int)
+    a_parser.add_argument('--checkpoints_dir', default='./checkpoints/')
+    # 锁定内存
+    a_parser.add_argument('--pin_mem', action='store_true', default=True)
+
+    # * Optimize参数
+    a_parser.add_argument('--opt', default='adamw', type=str, metavar='OPTIMIZER')
+    a_parser.add_argument('--opt-eps', default=1.0e-09, type=float, metavar='EPSILON')
+    a_parser.add_argument('--opt-betas', default=None, type=float, nargs='+', metavar='BETA')
+    a_parser.add_argument('--clip-grad', type=float, default=None, metavar='NORM')
+    a_parser.add_argument('--momentum', type=float, default=0.9, metavar='M')
+    a_parser.add_argument('--weight-decay', type=float, default=0.01)
+
+    # * Learning rate 参数
+    a_parser.add_argument('--sched', default='cosine', type=str, metavar='SCHEDULER')
+    a_parser.add_argument('--lr', type=float, default=1.0e-4, metavar='LR')
+    a_parser.add_argument('--lr-noise', type=float, nargs='+', default=None, metavar='pct, pct')
+    a_parser.add_argument('--lr-noise-pct', type=float, default=0.67, metavar='PERCENT')
+    a_parser.add_argument('--lr-noise-std', type=float, default=1.0, metavar='STDDEV')
+    a_parser.add_argument('--warmup-lr', type=float, default=1e-6, metavar='LR')
+    a_parser.add_argument('--min-lr', type=float, default=1.0e-08, metavar='LR')
+    a_parser.add_argument('--decay-epochs', type=float, default=30, metavar='N')
+    a_parser.add_argument('--warmup-epochs', type=int, default=0, metavar='N')
+    a_parser.add_argument('--cooldown-epochs', type=int, default=10, metavar='N')
+    a_parser.add_argument('--patience-epochs', type=int, default=10, metavar='N')
+    a_parser.add_argument('--decay-rate', '--dr', type=float, default=0.1, metavar='RATE')
+
+    a_parser.add_argument('--dataset', default='How2SignDataset', type=str,
+                          choices=['How2SignDataset', 'P14TDataset', 'CSLDailyDataset'])
+
+    return a_parser
+
+
+def main(args_, config):
+    args = vars(args_)
+    # 获取设备
+    device = torch.device(args['device'])
+    print("starting on...", device, sep=' ')
+
+    # 设置随机种子
+    seed = args['seed']
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    # 加载预训练模型的分词器
+    # tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    # args['vocab_size'] = tokenizer.vocab_size
+    # print('tokenizer.vocab_size', tokenizer.vocab_size)
+    # tokenizer = MBartTokenizer.from_pretrained("facebook/mbart-large-cc25")
+
+    # 加载训练数据集
+    # 训练数据集
+    train_data = eval(args['dataset'])(path=config[args['dataset']]['train_label_path'],
+                                       tokenizer=tokenizer,
+                                       config=config,
+                                       args=args,
+                                       phase='train')
+    train_dataloader = DataLoader(train_data,
+                                  batch_size=args['batch_size'],
+                                  num_workers=args['num_workers'],
+                                  collate_fn=train_data.collate_fn,
+                                  pin_memory=args['pin_mem'],
+                                  drop_last=True)
+
+    # 验证数据集
+    val_data = eval(args['dataset'])(path=config[args['dataset']]['dev_label_path'],
+                                     tokenizer=tokenizer,
+                                     config=config,
+                                     args=args,
+                                     phase='val')
+    val_dataloader = DataLoader(val_data,
+                                batch_size=args['batch_size'],
+                                num_workers=args['num_workers'],
+                                collate_fn=val_data.collate_fn,
+                                pin_memory=args['pin_mem'],
+                                drop_last=True)
+
+    # SLP Model
+    model = EmoGene()
+
+    # 移动到设备上
+    # model.to(device)
+
+    optimizer = create_optimizer(args_, model)
+    criterion = MAELoss()
+    scaler = GradScaler()
+
+    lr_scheduler = scheduler.CosineAnnealingLR(optimizer, eta_min=args['min_lr'], T_max=args['epochs'])
+
+    best_loss = float('inf')
+    for epoch in range(args['epochs']):
+        torch.cuda.empty_cache()
+        train_loss = train_one_epoch(model,
+                                     train_dataloader,
+                                     optimizer,
+                                     criterion,
+                                     device,
+                                     scaler)
+        print('train_loss: ', train_loss)
+
+        # TUDO
+
+        val_loss = evaluate(model, val_dataloader,
+                            criterion, device)
+
+
+def train_one_epoch(model, dataloader, optimizer, criterion, device, scaler):
+    model.train()
+    running_loss = 0.0
+
+    for step, batch in enumerate(dataloader):
+        print('---step---: ', step)
+        try:
+            optimizer.zero_grad()
+            src_input, tgt_input = batch
+            with autocast():
+                out = model(src_input, tgt_input)
+                # # 输出调整
+                x_values = out[:, 0::3, :]
+                y_values = out[:, 1::3, :]
+                p_values = out[:, 2::3, :]
+
+                scale = 800
+                # # 缩放x_values和y_values到 0到scale 的范围
+                x_values = torch.abs(x_values) * (scale / torch.max(torch.abs(x_values)))
+                y_values = torch.abs(y_values) * (scale / torch.max(torch.abs(y_values)))
+
+                # # 保留三位小数
+                x_values = torch.round(x_values * 1000) / 1000
+                y_values = torch.round(y_values * 1000) / 1000
+
+                # # 将p_values限制在0到1之间
+                p_values = torch.sigmoid(p_values)
+                p_values = torch.clamp(p_values, 0, 1)
+
+                # # 可以选择将x_values, y_values, p_values重新组合到out中
+                out = torch.cat((x_values, y_values, p_values), dim=1)
+                predicted_pose = out[:, :, torch.arange(out.size(-1)) % 3 != 2]
+                target_pose = tgt_input['input_ids']
+
+                print('predicted_pose', predicted_pose.shape)
+                print('target_pose', target_pose.shape)
+                loss = criterion(predicted_pose, target_pose)
+                print('loss', loss)
+                # 反向传播和梯度更新
+                scaler.scale(loss).backward()
+
+                # 梯度裁剪
+                print('here1')
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+                print('here')
+                scaler.step(optimizer)
+                scaler.update()
+
+                # 计算损失
+                running_loss += loss.item() * src_input['input_ids'].size(0)
+
+        except Exception as e:
+            print("数据错误，摒弃本数据。", e)
+            continue
+
+    epoch_loss = running_loss / len(dataloader.dataset)
+    return epoch_loss
+
+
+def evaluate(model, dataloader, criterion, device):
+    model.eval()
+    running_loss = 0.0
+    with torch.no_grad():
+        for step, batch in enumerate(dataloader):
+            print('---step---: ', step)
+            try:
+                src_input, tgt_input = batch
+                with autocast():
+                    out = model(src_input, tgt_input)
+                    print('val_out.shape', out.shape)
+                    # 计算评价指标
+            except Exception as e:
+                print("数据错误，摒弃本数据。", e)
+                continue
+
+    return running_loss
+
+
+if __name__ == '__main__':
+    # 显式启用并行化
+    os.environ["TOKENIZERS_PARALLELISM"] = "true"
+
+    # 加载参数
+    parser = argparse.ArgumentParser('SLP scripts', parents=[get_args_parser()])
+    args_ = parser.parse_args()
+    args = vars(args_)
+    config = utils.load_config(args['config'])
+
+    # 创建输出文件夹
+    if args['checkpoints_dir']:
+        Path(args['checkpoints_dir']).mkdir(parents=True, exist_ok=True)
+
+    # 开始训练
+    main(args_, config)
