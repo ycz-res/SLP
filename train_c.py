d321e663f2b052bb83d2ff4e516386dd69daf701
SapereAude <kang_hui1314@126.com>
2025-03-12 15:18:58 +0800

    empty

--- train_c.py
+++ train_c.py
@@ -302,6 +302,7 @@ def evaluate(slp_model, val_model, dataloader, criterion, device, tokenizer):
                     step_emo_score = criterion(kp_ids[:, :, -1], tgt_input['input_ids'][:, :, -1])
                     emo_scores += step_emo_score.item()
                     vocab_logits = val_model(kp_ids[:, :, :-1], tgt_input['attention_mask'], src_input)
+                    vocab_logits = src_input['input_ids']
 
                     # 计算评价指标
                     hypotheses_batch = tokenizer.batch_decode(vocab_logits.argmax(dim=-1), skip_special_tokens=True)
