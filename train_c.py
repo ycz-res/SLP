d1f7567707713423f3cc6cbfb52d80fa5481655a
SapereAude <kang_hui1314@126.com>
2025-03-13 14:46:00 +0800

    adjust

--- train_c.py
+++ train_c.py
@@ -65,7 +65,7 @@ def get_args_parser():
 
     a_parser.add_argument('--alpha', type=float, default=0, metavar='RATE')
     a_parser.add_argument('--prob', type=float, default=1, metavar='RATE')
-    a_parser.add_argument('--noise_level', type=float, default=0.35, metavar='RATE')
+    a_parser.add_argument('--noise_level', type=float, default=0.1, metavar='RATE')
 
     a_parser.add_argument('--dataset', default='PH14TDataset', type=str,
                           choices=['P2SASLDataset', 'PH14TDataset'])
