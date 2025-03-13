8c7530fc78bdcba3f05ce9ab0667879a8c9daec8
SapereAude <kang_hui1314@126.com>
2025-03-13 19:14:43 +0800

    adjust log func

--- params_validate.py
+++ params_validate.py
@@ -13,14 +13,40 @@ def frange(start, stop, step):
         start += step
 
 
-# PH14TDataset 数据集 不同p、n设置对实验的影响
-def ph14t_pn_test():
+# P2SASLDataset 数据集 不同p、n设置对实验的影响
+def p2s_pn_test():
     p_values = [round(x, 2) for x in frange(0.36, 0.52, 0.01)]
     n_values = [round(x, 2) for x in frange(0.05, 0.15, 0.01)]
     for p in p_values:
         for n in n_values:
             print(f"Running training with p/n: {p}/{n}")
-            log('params_c', p=p, n=n)
+            log('params_c', p=p, n=n, dataset='P2SASLDataset')
+
+            result = subprocess.run([
+                "python", "train_c.py",
+                "--dataset", "P2SASLDataset",
+                "--prob", str(p),
+                "--noise_level", str(n),
+            ])
+
+            # 检查是否执行成功
+            if result.returncode == 0:
+                print(f"Finished training with p/n: {p}/{n}")
+                log(phase='pl_c')
+            else:
+                print(f"Error occurred while training with p/n: {p}/{n}")
+                log(phase='params_c', p=p, n=n, msg='error')
+                return  # 如果出错，终止后续的训练
+
+
+# PH14TDataset 数据集 不同p、n设置对实验的影响
+def ph14t_pn_test():
+    p_values = [round(x, 2) for x in frange(0.15, 0.27, 0.01)]
+    n_values = [round(x, 2) for x in frange(0.05, 0.15, 0.01)]
+    for p in p_values:
+        for n in n_values:
+            print(f"Running training with p/n: {p}/{n}")
+            log('params_c', p=p, n=n, dataset='PH14TDataset')
 
             result = subprocess.run([
                 "python", "train_c.py",
@@ -37,9 +63,6 @@ def ph14t_pn_test():
                 print(f"Error occurred while training with p/n: {p}/{n}")
                 log(phase='params_c', p=p, n=n, msg='error')
                 return  # 如果出错，终止后续的训练
-
-
-# P2SASLDataset
 
 
 if __name__ == "__main__":
